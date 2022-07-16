f953dd102a897140515c55ae4489db1e6f1ff7de
vecnazmaga <103976145+vecnazmaga@users.noreply.github.com>
2022-07-16 03:22:44 -0400

    file add utils/redis

---
+++ utils/redis.js
@@ -0,0 +1,13 @@
+const redis = require("redis");
+const logger = require("../config/logger");
+const redisClient = redis.createClient({ url: 'redis://redis:6379' });
+
+redisClient.on("error", function (err) {
+  logger.error("Redis Client Error " + err);
+});
+
+redisClient.connect("error", function (err) {
+  logger.error("Redis Client Error " + err);
+});
+
+module.exports = redisClient;
